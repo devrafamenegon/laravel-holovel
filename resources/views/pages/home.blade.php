f2924b0cce0d3e9c6a737994c6ea1519410e721f
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-05-19 22:58:14 -0300

    :construction: Home page and layouts

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('/layouts/main')
+
+@section('content')
+
+<header class="bg-primary">
+    <div class="jumbotron bg-transparent text-center">
+        <div class="container">
+            <h1 class="display-3 text-pacifico">Album de fotos</h1> <br>
+            <p class="lead text-white-50">Something short and leading about the collection below—its contents, the creator,
+                etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p> <br>
+            <form action=""> <input class="form-control form-control-lg" type="text" name="" id="" placeholder="Digite aqui a foto que deseja pesquisar"> </form> <br>
+        </div>
+    </div>
+</header>
+
+@endsection
