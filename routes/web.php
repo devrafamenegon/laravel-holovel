ba6b7ad100e69c297abb69d4e79aef5c75144f4e
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-05-26 23:31:00 -0300

    :construction: Placeholder layout for now

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/home');
+});
+
+Route::get('/photos/new', function () {
+    return view('pages/photo_form');
 });
