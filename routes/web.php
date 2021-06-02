7e7ee413d4cb8e6b3bb8372bd95a4ba4743e62d4
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-06-02 15:12:34 -0300

    :hammer: Create Photo migration, Model and Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages/home');
-});
+Route::get('/', [PhotoController::class, 'index']);
 
 Route::get('/photos/new', function () {
-    return view('pages/photo_form');
+  return view('pages/photo_form');
 });
